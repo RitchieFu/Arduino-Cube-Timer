ef4c4e81fa6397a06601e7d47a284999169048f6
RitchieFu <64185930+RitchieFu@users.noreply.github.com>
2020-12-05 22:22:23 -0800

    Update CubeTimer.ino
    code cleanup

--- CubeTimer.ino
+++ CubeTimer.ino
@@ -1,27 +1,26 @@
-const int trigPin = 12;         
 const int echoPin = 13;
+const int trigPin = 12;   
 const int speakerPin =  10;
-const int switchPin = 7;   
+const int greenLED = 9;  
 const int redLED = 8;
-const int greenLED = 9;
+const int switchPin = 7; 
 const int yellowButton = 5;
-float distance = 0;           
 
 void setup() {
-
-  Serial.begin (9600);   
-  pinMode(trigPin, OUTPUT);  
+  Serial.begin(9600);   
   pinMode(echoPin, INPUT);
+  pinMode(trigPin, OUTPUT);  
   pinMode(speakerPin, OUTPUT);
-  pinMode(switchPin, INPUT_PULLUP);
-  pinMode(redLED, OUTPUT);
   pinMode(greenLED, OUTPUT);
+  pinMode(redLED, OUTPUT);
+  pinMode(switchPin, INPUT_PULLUP);
   pinMode(yellowButton, INPUT_PULLUP);
 }
 
+float distance = 0;      
+bool reset = true;     
 bool idle = true;
 bool armed = false;
-bool reset = true;
 int held_time = 0;
 unsigned long start_time = 0;
 unsigned long stop_time = 0;
@@ -32,11 +31,11 @@ void loop() {
   distance = getDistance();   
 
   if (digitalRead(yellowButton) == LOW) {
-    reset = true;
+    reset = true; 
   }
 
-  if (reset == true){
-    
+  if (reset == true){ 
+
     if (distance < 5) {
       
       if (idle == true) {
@@ -45,37 +44,41 @@ void loop() {
         armed = true;
         idle = false;
       }
+      
       if (idle == false and armed == false) {
-        held_time = 0;
         stop_time = millis();
         elapsed_time = stop_time - start_time;
         Serial.println(elapsed_time);
-        idle = true;
-        reset = false;
         
         if (digitalRead(7)== LOW) {
           tone(speakerPin, 247, 100);
         }
+        
         digitalWrite(redLED, HIGH);
         delay(100);
         digitalWrite(redLED, LOW);
-        delay(900);
+
+        reset = false;
+        idle = true;
+        held_time = 0;
       }
-      held_time += 100;
-      if (held_time >= 500) {
+      
+      held_time += 1;
+      if (held_time >= 5) {
         digitalWrite(greenLED, HIGH);
-        if (held_time == 500 && digitalRead(7) == LOW) {
+        if (held_time == 5 && digitalRead(7) == LOW) {
             tone(speakerPin, 294, 100);
         }
       }
     }
+    
     else {
       digitalWrite(redLED, LOW);
       digitalWrite(greenLED, LOW);
       if (millis() - trigger_time < 500) {
         held_time = 0;
-        armed = false;
         idle = true;
+        armed = false;
       }
       else if (idle == false and armed == true) {
         start_time = millis();
@@ -86,8 +89,7 @@ void loop() {
   delay(100);
 }
 
-float getDistance()
-{
+float getDistance(){
   float echoTime;                   
   float calculatedDistance;      
 
